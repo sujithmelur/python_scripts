fc367afb504126e8738f66f40833c91182c7fc54
sujithmelur <sujithmelur@gmail.com>
2019-05-28 20:19:59 +0530

    SG description with associated SG is working in sg_region_test.py

---
+++ aws_scripts/sg_region_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import boto3
+from botocore.exceptions import ClientError
+
+#ec2 = boto3.client('ec2')
+
+available_regions=['us-east-2', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-northeast-3', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ca-central-1', 'cn-north-1', 'cn-northwest-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'sa-east-1', 'us-gov-east-1', 'us-gov-west-1']
+#available_regions=['us-east-2', 'us-east-1']
+resource=list(raw_input("Enter the resource name seperated by comas : ").split(','))
+
+#def findRegion(a):
+# global test
+ #print "--",a
+for x in resource:
+   #print x
+   for region in available_regions:
+      try:
+	#print "1for"
+        ec2 = boto3.client('ec2',region_name = region)
+        response = ec2.describe_security_groups(GroupIds = [x])
+	if True:
+          print "**********************************************************************"
+          print "**********************************************************************"
+          print "Resource name is :"+x
+          print "Resource region is : "+region
+          for i in response['SecurityGroups']:
+	     #print "2for"
+             #print "Security Group name  :"+i['GroupName']
+             #print "Security Group Id  :"+i['GroupId']
+             print "Description :"+i['Description']
+             print "Ingress Details "
+             for j in i['IpPermissions']:
+               for k in j['IpRanges']:
+                 print "IP : %s   >> Port : %s >> Protocol : %s"%(k["CidrIp"],j['FromPort'],j['IpProtocol'])
+               for m in j['UserIdGroupPairs']:
+                 print "Additional SG id is %s"%m['GroupId']    
+		 if m['GroupId']:
+			resource.append(m['GroupId'])
+#	  	 test=list(m['GroupId'])
+	  #print response
+	  print "---"
+          break
+      except:
+        pass
+#findRegion(resource)
+#print test
+#findRegion(test)
---
+++ aws_scripts/vpc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+#ec2 = boto3.client('ec2')
+
+available_regions=['us-east-2', 'us-east-1', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-northeast-3', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ca-central-1', 'cn-north-1', 'cn-northwest-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'sa-east-1', 'us-gov-east-1', 'us-gov-west-1']
+#available_regions=['us-east-2', 'us-east-1']
+resource=list(raw_input("Enter the resource name seperated by comas : ").split(','))
+
+for x in resource:
+   for region in available_regions:
+      try:
+        ec2 = boto3.client('ec2',region_name = region)
+        response = ec2.describe_vpcs(VpcIds = [x])
+        if True:
+          print (json.dumps(response,sort_keys=True, indent=4))
+          break
+      except:
+        pass  
